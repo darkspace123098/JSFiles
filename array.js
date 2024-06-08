6db480697c0758d6c5bec987e7a1c8aa773210d8
Dark Space <150314290+darkspace123098@users.noreply.github.com>
2024-06-08 10:57:31 +0000

    array

---
+++ array.js
@@ -0,0 +1,35 @@
+// Basic Array Operations
+const myArr = [2,7,"first",10,true]
+// console.log(myArr);
+
+// myArr.push(5,"pushed text")
+// console.log(myArr);
+
+// myArr.pop()
+// myArr.pop()
+// myArr.pop()
+// myArr.shift()
+// myArr.unshift(9)
+// console.log(myArr);
+// console.log(myArr.length); 
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf("first"));
+// console.log(typeof myArr);
+
+// const newArr = myArr.join();
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// const sliceArray = myArr.slice(1,4)
+// console.log(sliceArray);
+// console.log(myArr);
+
+// const spliceArray = myArr.splice(1,4)
+// console.log(spliceArray);
+// console.log(myArr);
+
+
+// Advance Array Operations
+
+
